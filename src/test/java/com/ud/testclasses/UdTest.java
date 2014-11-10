eb76fe2ad8ce94b6f007eaf57658f3821400b4ac
Yudhvir Virk <udvdjm@gmail.com>
2014-11-10 14:26:24 +0530

    mm

---
+++ src/test/java/com/ud/testclasses/UdTest.java
@@ -0,0 +1,12 @@
+package com.ud.testclasses;
+
+import org.testng.annotations.Test;
+
+public class UdTest {
+
+	
+	@Test
+	public void test() {
+		System.out.println("ello-------->>>>>>>>>>>>");
+	}
+}
